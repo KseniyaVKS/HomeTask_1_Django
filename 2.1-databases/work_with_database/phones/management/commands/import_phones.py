faff4aac629158214303260da4c79cc49f405c39
Voskvitsova Kseniya <mksundrik@gmail.com>
2023-11-08 09:59:13 +0600

    Change files

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,4 +14,6 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            Phone(name=phone['name'], price=phone['price'], image=phone['image'], release_date=phone['release_date'], lte_exists=phone['lte_exists'], slug=
+            phone['name'].replace(' ', '_')).save()
+
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.CharField(max_length=100)
+    release_date = models.DateField()
+    lte_exists = models.CharField(max_length=10)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    # def __str__(self):
+    #     return self.name
+
