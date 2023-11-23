62ec874b577259da7c2ba6ca13649c1bdec3ad94
Voskvitsova Kseniya <mksundrik@gmail.com>
2023-11-23 09:36:52 +0600

    Change files

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
+
+from measurement.views import SensorView, MeasurementView, SensorDetailView
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorView.as_view()),
+    path('measurements/', MeasurementView.as_view()),
+    path('sensors/<pk>', SensorDetailView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
